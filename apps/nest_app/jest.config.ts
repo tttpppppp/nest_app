8278a7b84932239573f44b885cf271d3bc833c5a
Trần Tiến Phúc <112532197+tttpppppp@users.noreply.github.com>
2025-11-04 13:38:05 +0700

    Initial commit

---
+++ apps/nest_app/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'nest_app',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nest_app',
+};
